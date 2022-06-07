731535f2f0713604e1433a5f2d20cb911d5962bd
vinasar02 <103492157+vinasar02@users.noreply.github.com>
2022-06-07 17:31:08 +0800

    Add files via upload

---
+++ LearningSelenium/details.py
@@ -0,0 +1,11 @@
+#Details
+sysAdminuser = "SS001"
+sysAdminpass = "a9bb410c"
+NOCuser = "PLTest0004"
+tenantuser = "R00028"
+tenantpass = "983fda99"
+SVuser = "PLT3002"
+Mngmentuser = "C00313"
+FTuser = "PLT3001"
+
+Password = "Test1234"
