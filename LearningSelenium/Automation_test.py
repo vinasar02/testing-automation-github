63b08177b0b10109574262730cd28c6b78e7d85c
vinasar02 <103492157+vinasar02@users.noreply.github.com>
2022-06-08 10:38:33 +0800

    Delete LearningSelenium directory

--- LearningSelenium/Automation_test.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("https://fibersafe.tmrnd.com.my:86/")
-driver.maximize_window()
-print(driver.title)
